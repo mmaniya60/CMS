ca0a0044ef67fb3bc41f48014ad972a6904fad06
mmaniya60 <70863786+mmaniya60@users.noreply.github.com>
2021-12-07 04:27:23 -0600

    Added WYSIWYG textarea for movie description
    WYSIWYG Textarea for movie description and other minor bug fixed for adding empty genre.

--- admin/add.php
+++ admin/add.php
@@ -13,7 +13,6 @@
         if(isset($_POST['title'], $_POST['year'], $_POST['description'])){
             $title = $_POST['title'];
             $description = $_POST['description'];
-            $movie_genre = $_POST['genre'];
             $year = $_POST['year'];
 
             if(isset($_FILES['image'])){
@@ -29,7 +28,7 @@
                 }
             }
 
-            if(empty($title) || empty($description) || empty($movie_genre)){
+            if(empty($title) || empty($description) || empty($_POST['genre']) || $_POST['genre'] == "0"){
                 $error = " All fields are required!";
             }
             else if(in_array($file_ext, $extensions) === false){
@@ -39,14 +38,15 @@
 
                 $query = $db->prepare("INSERT INTO post (genre_id, movie_title, movie_year, movie_description, movie_image, posted_by) VALUES (?, ?, ?, ?, ?, ?);");
 
-                $query->bindValue(1, $movie_genre);
+                $query->bindValue(1, $_POST['genre']);
                 $query->bindValue(2, $title);
                 $query->bindValue(3, $year);
                 $query->bindValue(4, $description);
                 $query->bindValue(5, $file_name);
                 $query->bindValue(6, $_SESSION['logged_in']);
 
-                $query1 = $db->prepare("UPDATE genre SET post = post + 1 WHERE genre_id = $movie_genre");
+                $query1 = $db->prepare("UPDATE genre SET post = post + 1 WHERE genre_id = ?");
+                $query1->bindValue(1, $_POST['genre']);
 
 
                 $query->execute();
@@ -85,12 +85,13 @@
             <input class="w3-input"  type="text" name="title" placeholder="Movie Title" /><br /><br />
             <input class="w3-input" type="text" name="year" placeholder="Movie Year" /><br /><br />
             <select class="w3-select" name="genre">
-                <option value="" disabled selected>Select Genre</option>
+                <option value="0" disabled selected>Select Genre</option>
                 <?php foreach($genres as $genre): ?>
                     <option value="<?= $genre['genre_id'] ?>"><?= $genre['genres'] ?></option>
                 <?php endforeach ?>
             </select><br /><br />
-            <textarea class="w3-input w3-border" rows="20" cols="50" type="text" name="description" placeholder="Movie Review"></textarea><br /><br />
+            <label>Movie Description:</label><br />
+            <textarea id="description" name="description"></textarea><br /><br />
             <input type="file" name="image"/><br /><br />
             <input class="w3-button w3-green" type="submit" name="submit" value="Add Review" />
         </form>
@@ -98,6 +99,12 @@
 
         <a href="index.php">&larr; Back</a>
     </div>
+
+    <script src="ckeditor/ckeditor.js"></script>
+    <script>
+        CKEDITOR.replace('description');
+    </script>
+
 </body>
 </html>
 
