1df09a943b0568b879f7424c83eb39b9ff0d117d
mmaniya60 <70863786+mmaniya60@users.noreply.github.com>
2021-12-13 01:51:03 -0600

    Validated ID
    validated and sanitized all the GETs and POSTs

--- admin/users/delete_users.php
+++ admin/users/delete_users.php
@@ -7,8 +7,11 @@ include_once('../../includes/connect.php');
 
     if(isset($_SESSION['logged_in'])){
         if(isset($_GET['id'])){
-
-            $id = $_GET['id'];
+            $id = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+            if (!$id || empty($id)) {
+                echo '<script>alert("Incorrect ID passed!!!")</script>';
+                exit;
+            }
 
             $query = $db->prepare("DELETE FROM user WHERE user_id = ?");
             $query->bindValue(1, $id);
